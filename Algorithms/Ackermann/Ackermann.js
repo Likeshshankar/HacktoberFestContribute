ead9efa1f35d00ba91dcc50f157b5635a246bc32
Likeshshankar <likeshshankar20@gmail.com>
2024-10-19 09:09:09 +0530

    Update Ackermann.js

--- Algorithms/Ackermann/Ackermann.js
+++ Algorithms/Ackermann/Ackermann.js
@@ -1,10 +1,10 @@
-n = 2;
-m = 3;
+ let n = 2;
+let m = 3;
 
 function ackermann(n, m) {
-  if (n == 0) return m + 1;
-  if (m == 0) return ackermann(n - 1, 1);
+  if (n === 0) return m + 1;
+  if (n > 0 && m === 0) return ackermann(n - 1, 1);
   return ackermann(n - 1, ackermann(n, m - 1));
 }
 
-console.log(`ackermann(${n},${m}) = `, ack(n, m));
+console.log(`ackermann(${n}, ${m}) = `, ackermann(n, m));
